8d5b43483027eca80351438af19991fa91345429
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-07-31 16:42:55 +0200

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle of n"""
     if n <= 0:
         return []
     else:
