d27a66f296e99c246b1789f8f69f9c609a68abe1
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-07-31 16:39:31 +0200

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    else:
+        result = [[1]]
+        for i in range(1, n):
+            row = [1]
+            for j in range(1, i):
+                row.append(result[i - 1][j - 1] + result[i - 1][j])
+            row.append(1)
+            result.append(row)
+        return result
+        
+
+
+    
