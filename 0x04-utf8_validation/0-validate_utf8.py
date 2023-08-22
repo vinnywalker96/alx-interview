6b7307eba6b771e1636123ad59c85f37370e5340
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-22 15:19:33 +0200

    0x04. UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""UTF-8 Validation"""
+
+
+def validUTF8(data):
+    min_val = 0
+    max_val = 127
+    for val in data:
+        if not (min_val <= val <= max_val):
+            return False
+    return True
