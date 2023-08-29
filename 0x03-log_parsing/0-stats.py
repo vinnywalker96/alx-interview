50a0fb842580a209c970af928838b92f20fdccca
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-29 15:59:30 +0000

    log parse

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,32 +1,46 @@
 #!/usr/bin/python3
-"""Log parsing"""
-
+"""Log Parse"""
 import sys
 import collections
+import signal
 
 
-def log_stats():
-    total_size = 0
-    status_counts = collections.defaultdict(int)
+total_size = 0
+status_counts = collections.defaultdict(int)
+line_count = 0
 
+def print_statistics():
+    print(f"Fil file size: {total_size}")
+    for status in sorted(status_counts):
+        print(f"{status}: {status_counts[status]}")
+
+def handle_interrupt(signum, frame):
+    print_statistics()
+    sys.exit(0)
+
+# Register the signal handler for CTRL+C
+signal.signal(signal.SIGINT, handle_interrupt)
+
+try:
     for line in sys.stdin:
         parts = line.split()
-        if len(parts) >= 10 and parts[8].isnumeric():
-            id = parts[0]
-            date = parts[2][1:-1]
-            request = " ".join(parts[5:8])
-            status_code = int(parts[8])
-            file_size = int(parts[9])
 
-            if status_code.isdigit():
-                total_size += int(file_size)
-                status_counts[int(status_code)] += 1
-        
-            if len(status_counts) % 10 == 0:
-                print(f"File Size: {total_size}")
-                for status_code, count in sorted(status_counts.items()):
-                    print(f'{status_code}: {count}')
+        if len(parts) >= 7: 
+            status_code = parts[-2]
+            if status_code.isnumeric():
+                status_code = int(status_code)
+                if status_code in [200, 301, 400, 401, 403, 404, 405, 500]:
+                    file_size = int(parts[-1])
+                    total_size += file_size
+                    status_counts[status_code] += 1
+                    line_count += 1
+
+        if line_count % 10 == 0:
+            print_statistics()
+
+except KeyboardInterrupt:
+    pass
 
 
-if __name__ == "__main__":
-    log_stats()
+print_statistics()
+
