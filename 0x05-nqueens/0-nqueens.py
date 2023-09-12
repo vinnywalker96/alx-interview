2cabab18b3452ff8bb3119ef71aa02b740ba08df
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-12 18:44:19 +0200

    n queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""0-nqueens.py"""
+import sys
+
+def solveNQueens(n):
+    def could_place(row, col):
+        for i in range(row):
+            if board[i] == col or \
+                board[i] - i == col - row or \
+                board[i] + i == col + row:
+                return False
+        return True
+
+    def place_queens(n, row):
+        if row == n:
+            result.append(board[:])
+            return
+        for col in range(n):
+            if could_place(row, col):
+                board[row] = col
+                place_queens(n, row + 1)
+                board[row] = 0
+
+    result = []
+    board = [0] * n
+    place_queens(n, 0)
+    return [["." * i + "Q" + "." * (n - i - 1) for i in sol] for sol in result]
+
+if __name__ == "__main__":
+    print(solveNQueens(int(sys.argv[1])))
