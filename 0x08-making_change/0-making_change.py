0b1a065442f7fca9060d88aa86a37461ed4528c1
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-29 09:39:54 +0200

    calculate change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,12 +4,14 @@
 
 def makeChange(coins, total):
     """Calculate Change"""
-    if total <= 0:
-        return 0
+    if total < 0:
+        return -1
     res = [float('inf')] * (total + 1)
     res[0] = 0
     for coin in coins:
         for i in range(coin, total + 1):
             if res[i - coin] != float('inf'):
                 res[i] = min(res[i], res[i - coin] + 1)
-    return res[total] if res[total] != float('inf') else -1
+    if res[total] == float('inf'):
+        return -1
+    return res[total]
