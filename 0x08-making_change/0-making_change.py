edbe170a1fcf52cb62583e94d5c2ae53a0b0b748
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-10-03 12:47:26 +0200

    Making changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-"""Change comes from within"""
+"""Making Change"""
+import sys
 
 
 def makeChange(coins, total):
-    """Calculate Change"""
-    if total < 0:
+    """
+    Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    """
+    if total <= 0:
+        return 0
+    table = [sys.maxsize for i in range(total + 1)]
+    table[0] = 0
+    m = len(coins)
+    for i in range(1, total + 1):
+        for j in range(m):
+            if coins[j] <= i:
+                subres = table[i - coins[j]]
+                if subres != sys.maxsize and subres + 1 < table[i]:
+                    table[i] = subres + 1
+
+    if table[total] == sys.maxsize:
         return -1
-    res = [float('inf')] * (total + 1)
-    res[0] = 0
-    for coin in coins:
-        for i in range(coin, total + 1):
-            if res[i - coin] != float('inf'):
-                res[i] = min(res[i], res[i - coin] + 1)
-    if res[total] == float('inf'):
-        return -1
-    return res[total]
+    return table[total]
