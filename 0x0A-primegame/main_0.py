8621d8fc96722dcb2c656cb8e4a1426e63d36122
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-10-23 07:43:52 +0200

    Primes Game

--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+Main file for testing
+"""
 
 isWinner = __import__('0-prime_game').isWinner
 
+nums = [0] * 10000
+for i in range(10000):
+    nums[i] = i
 
-print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+print("Winner: {}".format(isWinner(10000, nums)))
